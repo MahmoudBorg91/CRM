19abca1912721e815aee357ca125b7edd32a34d6
MahmoudBorg91 <mahmoudborg@outlook.com>
2022-12-12 01:51:50 +0400

    client2

---
+++ GSI_Internal/wwwroot/Client/js/ClientDesignScript.js
@@ -0,0 +1,19 @@
+﻿var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 5,
+        spaceBetween: 30,
+        autoplaySpeed: 2000,
+        slidesPerGroup: 1,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+    }
+);
