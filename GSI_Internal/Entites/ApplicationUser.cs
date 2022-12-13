1790331dd88392d0ccdcb97f37cb0cf7ccbe6b39
MahmoudBorg91 <mahmoudborg@outlook.com>
2022-12-13 06:17:44 +0400

    UND

--- GSI_Internal/Entites/ApplicationUser.cs
+++ GSI_Internal/Entites/ApplicationUser.cs
@@ -6,7 +6,7 @@ namespace GSI_Internal.Entites
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        // 0-employee 1-client 2-company
         public int UserType { get; set; }
         
         public bool Status { get; set; } = true;
